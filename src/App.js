cde1565c062684f002369c840d1aa9dc67bc5358
sohee <topa11uu@gmail.com>
2024-05-02 02:08:40 +0900

    dev 브랜치 가져오기-2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ListPage from "./pages/ListPage/ListPage";
 import MainPage from "./pages/MainPage/MainPage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import PostPage from "./pages/postPage/PostPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/list" element={<ListPage />} />
+        <Route path="/post" element={<PostPage />} />
         <Route path="/*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
