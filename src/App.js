7ca08077a262a9ebfa0f39d2c24c8a8ffc713ee6
YeongTaek <yeongtaek2109@gmail.com>
2024-05-01 20:24:16 +0900

    페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ListPage from "./pages/ListPage/ListPage";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ListPage from './pages/ListPage/ListPage';
+import MainPage from './pages/MainPage/MainPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<MainPage />} />
         <Route path="/list" element={<ListPage />} />
+        <Route path="/*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
