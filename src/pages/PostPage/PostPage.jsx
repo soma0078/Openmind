8f4224f2ee52b01c8f72e698ad30411e56696b2f
YeongTaek <yeongtaek2109@gmail.com>
2024-05-03 20:56:37 +0900

    PostPage 임시구현

--- src/pages/PostPage/PostPage.jsx
+++ src/pages/PostPage/PostPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logoImage from '../../assets/img-logo.png';
 import achoImage from '../../assets/img-acho.png';
 import linkImage from '../../assets/icon-link.svg';
@@ -6,8 +6,14 @@ import kakaoImage from '../../assets/icon-kakaotalk.svg';
 import facebookImage from '../../assets/icon-facebook.svg';
 import messageImage from '../../assets/icon-messages.svg';
 import QuestionCard from './QuestionCard';
+import { Link } from 'react-router-dom';
 
 function PostPage() {
+  // const [cardList, setCardList] = useState([]); 카드데이터 받아올 떄, 사용하는 상태
+  const popUpModal = () => {
+   // 내용 작성
+  };
+
   return (
     <div className='flex flex-col h-[234px]'>
       <div className='bg-[#F9F9F9] bg-main1 bg-contain bg-no-repeat'>
@@ -28,13 +34,23 @@ function PostPage() {
             <img className='w-[24px] h-[24px]' src={messageImage} alt="메시지 이모티콘" />
             <span className='font-[400] text-[20px] text-[#542F1A]'>?개의 질문이 있습니다</span>
           </div>
-          <QuestionCard />
-          <QuestionCard />
+          {/* 카드 데이터를 받아오게끔 구현 
+          {cardList?.map((card) => (
+            <QuestionCard item={card} key={card.id} />
+          ))}
+          */}
           <QuestionCard />
         </div>
       </div>
+      <div className='relative flex justify-between bottom-[50px] px-[50px]'>
+        {/* 임시적으로 뒤로 가는 것처럼 구현. 다른 방법을 찾아야 함 */}
+        <Link to='/list'> 
+          <button className='rounded-[200px] py-[12px] px-[24px] bg-[#542F1A] text-[20px] text-[#FFFFFF] font-[400]' >뒤로 가기</button>
+        </Link>
+        <button className='rounded-[200px] py-[12px] px-[24px] bg-[#542F1A] text-[20px] text-[#FFFFFF] font-[400]' onClick={popUpModal}>질문 작성하기</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default PostPage
+export default PostPage;
