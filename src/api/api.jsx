24981265fcf358a41618ac0b8804e570ca71ae52
hyunow <stophyun_13@naver.com>
2024-05-04 11:57:22 +0900

    feat: ListPage 반응형- 아이템 개수

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,13 +1,17 @@
-const BASE_URL = 'https://openmind-api.vercel.app/6-13';
+const BASE_URL = "https://openmind-api.vercel.app/6-13";
 
 // 질문카드 불러오기
 export async function getSubjects(params = {}) {
+  // 현재 네트워크 request url 예시
+  // BASE_URL/subjects/?sort=createdAt&limit=8
+  // 페이지네이션 작업 시, 필요한 params를 더 추가하면 될 것 같습니다
+
+  // limit: 몇 개 받을 것인지
+  // offset: 몇 번부터 받을 것인지
   const query = new URLSearchParams(params).toString();
 
   try {
-    const response = await fetch(
-      `${BASE_URL}/subjects/?${query}`
-    );
+    const response = await fetch(`${BASE_URL}/subjects/?${query}`);
     if (!response.ok) {
       throw new Error(`HTTP error: ${response.status}`);
     }
@@ -19,24 +23,24 @@ export async function getSubjects(params = {}) {
     console.error("Failed to fetch products:", error);
     throw error;
   }
-};
+}
 
 // 질문카드 생성
 export const createQuestionCard = async (name) => {
   try {
     const response = await fetch(`${BASE_URL}/subjects/`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         name: name,
-        team: '13',
+        team: "13",
       }),
     });
 
     if (response.ok) return response.json();
-    return new Error('');
+    return new Error("");
   } catch (e) {
     if (e instanceof Error) return e;
   }
--- src/pages/ListPage/components/AllSubjectsSection.jsx
+++ src/pages/ListPage/components/AllSubjectsSection.jsx
@@ -3,6 +3,7 @@ import UserCard from "./UserCard.jsx";
 import { getSubjects } from "../../../api/api.jsx";
 import DropdownMenu from "./DropdownMenu.jsx";
 
+// tailwind media query 적용 시 참고
 // md (min-width: 768px)
 // xl (min-width: 1280px)
 
@@ -21,13 +22,18 @@ const getPageSize = () => {
 };
 
 function AllSubjectsSection() {
-  const [sort, setSort] = useState("name");
-  const [page, setPage] = useState(1);
-  const [pageSize, setPageSize] = useState(getPageSize());
-  const [subjectList, setSubjectList] = useState([]);
+  // 안 쓰는 부분은 일단 주석 처리 해두겠습니다
+  // const [page, setPage] = useState(1);
+  // const [pageSize, setPageSize] = useState(getPageSize());
 
-  const fetchSortedData = async ({ sort, page, pageSize }) => {
-    const subjects = await getSubjects({ sort, page, pageSize });
+  const [subjectList, setSubjectList] = useState([]);
+  const [sort, setSort] = useState("name");
+  const [limit, setLimit] = useState(getPageSize());
+
+  // 일단 저는 sort, limit만 아규먼트로 전달해주었습니다
+  // 페이지네이션 적용 시에 page, pageSize, ... 필요할 것으로 보입니다
+  const fetchSortedData = async ({ sort, limit }) => {
+    const subjects = await getSubjects({ sort, limit });
     setSubjectList(subjects.results);
   };
 
@@ -37,32 +43,36 @@ function AllSubjectsSection() {
 
   useEffect(() => {
     const handleResize = () => {
-      setPageSize(getPageSize());
+      // setPageSize(getPageSize());
+      setLimit(getPageSize());
     };
 
     // 화면 크기 변경할 때마다 pageSize를 다시 계산해 넣음
     window.addEventListener("resize", handleResize);
-    fetchSortedData({ sort, page, pageSize });
+    fetchSortedData({ sort, limit });
 
     // Cleanup function
     return () => {
       window.removeEventListener("resize", handleResize);
     };
-  }, [sort, page, pageSize]);
+  }, [sort, limit]);
 
-  const onPageChange = (pageNumber) => {
-    setPage(pageNumber);
-  };
+  // const onPageChange = (pageNumber) => {
+  //   setPage(pageNumber);
+  // };
 
   return (
-    <div className="flex flex-col gap-[40px]">
-      <div className="z-20 pt-[40px] flex flex-col gap-[20px] justify-center items-center">
-        <p className="w-[341px] h-[48px] text-[40px] font-normal">
+    <div className="flex flex-col gap-[16px] md:gap-[40px]">
+      <div className="z-20 pt-[40px] flex md:flex-col gap-[20px] justify-center items-center">
+        <p className="w-[341px] h-[48px] text-[24px] md:text-[40px] font-normal">
           누구에게 질문할까요?
         </p>
         <DropdownMenu onSortSelection={handleSortSelection} />
       </div>
-      <div className="w-[940px] h-[394px] grid grid-cols-2  md:grid-cols-3 xl:grid-cols-4 gap-[20px]">
+      <div
+        className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 
+      gap-[16px] md:gap-[20px]"
+      >
         {subjectList?.map((subject) => (
           <UserCard item={subject} key={subject.id} />
         ))}
