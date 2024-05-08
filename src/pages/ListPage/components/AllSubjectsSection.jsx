28284c0dd2644154088ce8c4788a544d8ad87476
hyunow <stophyun_13@naver.com>
2024-05-08 22:16:17 +0900

    fix: conflict 해결

--- src/pages/ListPage/components/AllSubjectsSection.jsx
+++ src/pages/ListPage/components/AllSubjectsSection.jsx
@@ -13,9 +13,12 @@ const getPageSize = () => {
   if (width < 768) {
     // Mobile viewport
     return 6;
-  } else if (width < 1280) {
-    // Tablet viewport
+  } else if (width < 868) {
+    // Tablet-1 viewport
     return 6;
+  } else if (width < 1280) {
+    // Tablet-2 viewport
+    return 8;
   } else {
     // Desktop viewport
     return 8;
@@ -29,6 +32,15 @@ function AllSubjectsSection() {
   const [totalPageNum, setTotalPageNum] = useState(0);
   const [subjectList, setSubjectList] = useState([]);
   const [sort, setSort] = useState('createdAt');
+  const [limit, setLimit] = useState(getPageSize());
+
+  // 일단 저는 sort, limit만 아규먼트로 전달해주었습니다
+  // 페이지네이션 적용 시에 page, pageSize, ... 필요할 것으로 보입니다
+  const fetchSortedData = async ({ sort, limit }) => {
+    const subjects = await getSubjects({ sort, limit });
+    setSubjectList(subjects.results);
+    setTotalPageNum(Math.ceil(subjects.count / pageSize));
+  };
 
   const handleSortSelection = (sortOption) => {
     setSort(sortOption);
@@ -44,15 +56,7 @@ function AllSubjectsSection() {
 
     // 화면 크기 변경할 때마다 pageSize를 다시 계산해 넣음
     window.addEventListener('resize', handleResize);
-
-    // 기존의 fetchSortData가 useEffect 밖에 있고 호출만 안에 있었는데 같이 넣었습니다.
-    const fetchData = async () => {
-      const subjects = await getSubjects({ sort, page, pageSize });
-      setSubjectList(subjects.results);
-      setTotalPageNum(Math.ceil(subjects.count / pageSize));
-    };
-
-    fetchData();
+    fetchSortedData({ sort, limit });
 
     // Cleanup function
     return () => {
@@ -65,19 +69,19 @@ function AllSubjectsSection() {
   };
 
   return (
-    <div className="flex flex-col gap-[16px] md:gap-[40px]">
-      <div className="z-20 pt-[40px] flex md:flex-col gap-[20px] justify-center items-center">
-        <p className="w-[341px] h-[48px] text-[24px] md:text-[40px] font-normal">
+    <div className="flex flex-col justify-center items-center gap-[16px] tablet-1:gap-[40px]">
+      <div className="z-20 px-[20px] pt-[40px] flex tablet-1:flex-col gap-[40px] tablet-1:gap-[20px] justify-between items-center">
+        <p className="w-[214px] tablet-1:w-[341px] text-[24px] tablet-1:text-[40px] font-normal">
           누구에게 질문할까요?
         </p>
         <DropdownMenu onSortSelection={handleSortSelection} />
       </div>
       <div
-        className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 
-      gap-[16px] md:gap-[20px]"
+        className="w-[327px] tablet-1:w-[700px] tablet-2:w-[940px] tablet-2:min-w-[804px] pc:w-[940px] grid grid-cols-2 tablet-1:grid-cols-3 tablet-2:grid-cols-4 pc:grid-cols-4 
+      gap-[16px] tablet-1:gap-[20px]"
       >
         {subjectList?.map((subject) => (
-          <UserCard item={subject} key={subject.id} />
+          <UserCard className="" item={subject} key={subject.id} />
         ))}
       </div>
       <PaginationBar
