22744d9258cd6326318242f292af060bc2b59219
YeongTaek <yeongtaek2109@gmail.com>
2024-05-10 19:53:19 +0900

    refectoring : div태그 삭제

--- src/pages/PostPage/components/QuestionList.jsx
+++ src/pages/PostPage/components/QuestionList.jsx
@@ -5,16 +5,15 @@ function QuestionList({ questionData }) {
   if (!questionData || !questionData.length === 0) {
     return <div>No questions to display</div>; // 질문이 없는 경우 메시지를 출력하거나 아무것도 렌더링하지 않습니다.
   }
+
   return (
-    <div>
-      <ul>
-        {questionData.map((question) => (
-          <li key={question.id}>
-            <QuestionCard question={question} />
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul>
+      {questionData.map((question) => (
+        <li key={question.id}>
+          <QuestionCard question={question} />
+        </li>
+      ))}
+    </ul>
   );
 }
 
