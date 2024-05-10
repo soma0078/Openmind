27e1cfff8fa2e5b520c1f8c9b700e475e78fdf66
soma0078 <104878713+soma0078@users.noreply.github.com>
2024-05-10 12:41:56 +0900

    feat: PostPage에서 무한 스크롤 기능 구현

--- src/api/api.js
+++ src/api/api.js
@@ -111,7 +111,7 @@ export async function submitQuestion(id, questionContent) {
 }
 
 // 주어진 ID를 사용해 질문 데이터를 가져오는 함수
-export async function getQuestionsByUserId(subject_id, offset = 0, limit = 5) {
+export async function getQuestionsByUserId(subject_id, offset, limit) {
   try {
     const questionQuery = `offset=${offset}&limit=${limit}`;
     const response = await fetch(
@@ -129,13 +129,23 @@ export async function getQuestionsByUserId(subject_id, offset = 0, limit = 5) {
 }
 
 // 사용자 데이터를 기반으로 질문 데이터를 가져와 상태를 설정하는 함수
-export async function fetchQuestionsByUser(userData) {
+export async function fetchQuestionsByUser(userData, offset, limit) {
   try {
     if (!userData || !userData.id) {
       console.error('사용자 데이터 또는 사용자 ID를 가져올 수 없습니다.');
-      return null; // null을 반환하여 호출자에게 알림
+      return null;
     }
-    const fetchedQuestionData = await getQuestionsByUserId(userData.id);
+
+    // 이전에 이미 가져온 데이터가 있다면, 새로운 데이터를 가져오지 않고 그대로 반환합니다.
+    if (userData.questions) {
+      return userData.questions.slice(offset, offset + limit);
+    }
+
+    const fetchedQuestionData = await getQuestionsByUserId(
+      userData.id,
+      offset,
+      limit,
+    );
     if (fetchedQuestionData && Array.isArray(fetchedQuestionData.results)) {
       // results를 사용하여 배열인지 확인
       return fetchedQuestionData.results; // 배열 반환
