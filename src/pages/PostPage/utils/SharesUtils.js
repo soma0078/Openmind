9df16fdb85d278cccc8442264866c3046a43956a
YeongTaek <yeongtaek2109@gmail.com>
2024-05-10 10:06:48 +0900

    feat : Kakao share

--- src/pages/PostPage/utils/SharesUtils.js
+++ src/pages/PostPage/utils/SharesUtils.js
@@ -10,28 +10,59 @@ export const shareFacebook = () => {
 };
 
 //Kakao
-export const shareKakao = (url) => {
-  if (!window.Kakao.isInitialized()) {
-    const key = process.env.REACT_APP_JAVASCRIPT_KEY;
-    window.Kakao.init(key);
-  }
+const { Kakao } = window;
 
-  window.Kakao.Share.sendDefault({
+export const shareKakao = () => {
+  Kakao.Share.sendDefault({
     objectType: 'feed',
     content: {
       title: 'Openmind',
       description: '#Openmind #질문 #소통 #무물보',
       imageUrl:
-        'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbE0AdB%2FbtsDW9qmIRy%2FMrUAV7zrpnKKgAHPbzkOd1%2Fimg.png',
+        'http://k.kakaocdn.net/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
       link: {
-        mobileWebUrl: url,
-        webUrl: url,
+        mobileWebUrl: 'https://developers.kakao.com',
+        webUrl: 'https://developers.kakao.com',
       },
     },
-    social: {
-      likeCount: 6,
-      commnetCount: 74,
-      sharedCount: 921,
-    },
+    //    social: {
+    //       likeCount: 6,
+    //       commnetCount: 74,
+    //       sharedCount: 921,
+    buttons: [
+      {
+        title: '나도 테스트 하러가기',
+        link: {
+          mobileWebUrl: 'https://developers.kakao.com',
+        },
+      },
+    ],
   });
 };
+
+// }
+// export const shareKakao = (url) => {
+//   if (!window.Kakao.isInitialized()) {
+//     const key = process.env.REACT_APP_JAVASCRIPT_KEY;
+//     window.Kakao.init(key);
+//   }
+
+//   window.Kakao.Share.sendDefault({
+//     objectType: 'feed',
+//     content: {
+//       title: 'Openmind',
+//       description: '#Openmind #질문 #소통 #무물보',
+//       imageUrl:
+//         'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbE0AdB%2FbtsDW9qmIRy%2FMrUAV7zrpnKKgAHPbzkOd1%2Fimg.png',
+//       link: {
+//         mobileWebUrl: url,
+//         webUrl: url,
+//       },
+//     },
+//     social: {
+//       likeCount: 6,
+//       commnetCount: 74,
+//       sharedCount: 921,
+//     },
+//   });
+// };
