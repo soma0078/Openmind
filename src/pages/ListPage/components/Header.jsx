6450913585bfb88c1b3d8a10e0f35a514e7d0874
YeongTaek <yeongtaek2109@gmail.com>
2024-05-13 18:26:27 +0900

    test : 답변하러 가기

--- src/pages/ListPage/components/Header.jsx
+++ src/pages/ListPage/components/Header.jsx
@@ -1,12 +1,18 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 function Header() {
+  const nav = useNavigate();
+  const { postId } = useParams();
+  const onMoveIndivisualPeed = () => {
+    nav(`/post/${postId}`);
+  };
   return (
     <div className="pt-[50px] flex justify-center tablet-1:justify-start tablet-1:pl-[50px]">
       <Link to="/">
         <img className="w-[146px] h-[57px]" src={logo} alt="오픈마인드 로고" />
       </Link>
+      <button onClick={onMoveIndivisualPeed}>답변하러 가기</button>
     </div>
   );
 }
